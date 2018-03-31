a6c43c86d35029fa73aa1161321736bdc3124e2f
ManfredSteyer <manfred.steyer@gmx.net>
2018-03-31 20:22:12 +0200

    fin

--- demo-app/src/app/core/core.module.ts
+++ demo-app/src/app/core/core.module.ts
@@ -6,7 +6,11 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
   imports: [
     CommonModule
   ],
-  declarations: [SideMenuComponent],
-  exports: [SideMenuComponent]
+  declarations: [
+    SideMenuComponent
+  ],
+  exports: [
+    SideMenuComponent
+  ]
 })
 export class CoreModule { }
